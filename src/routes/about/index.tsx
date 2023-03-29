2470e775aa01b143839dcff454e90adfac48bc06
mark <markpackham1@gmail.com>
2023-03-29 11:56:39 +0100

    demo a global style in a component
    https://youtu.be/vxhXd1mz1gs?t=277

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -1,6 +1,9 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStyles$ } from "@builder.io/qwik";
+import AboutStyles from "./about.css?inline";
 
 export default component$(() => {
+  useStyles$(AboutStyles);
+
   return (
     <article>
       <h2>About</h2>
