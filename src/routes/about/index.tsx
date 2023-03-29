2b06244ca6c80041049a17e7d01690f4e0c34ce5
mark <markpackham1@gmail.com>
2023-03-29 11:44:17 +0100

    create contact section and remove demo section
    https://youtu.be/Ti2BoRVp-kY?t=276

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -1,5 +1,28 @@
 import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
-  return <div>About Page</div>;
+  console.log("Hello");
+  return (
+    <article>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore odio
+        cupiditate consectetur dignissimos et minima eum natus fuga doloribus,
+        earum, aspernatur eius porro vitae veritatis ab iste accusamus delectus
+        fugiat!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore odio
+        cupiditate consectetur dignissimos et minima eum natus fuga doloribus,
+        earum, aspernatur eius porro vitae veritatis ab iste accusamus delectus
+        fugiat!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore odio
+        cupiditate consectetur dignissimos et minima eum natus fuga doloribus,
+        earum, aspernatur eius porro vitae veritatis ab iste accusamus delectus
+        fugiat!
+      </p>
+    </article>
+  );
 });
