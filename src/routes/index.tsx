36be8edfbb157248cb1bdd7de3da7bd84fa4a401
mark <markpackham1@gmail.com>
2023-03-30 14:52:59 +0100

    demo useStore
    https://youtu.be/N624vxHocDc?t=232

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { component$, useSignal, useStore } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   const name = useSignal("Bob");
 
+  const person = useStore({ name: "Peach", age: 30 });
+
   return (
     <>
       <h2>Hello World</h2>
       <p>Hello {name.value}</p>
+      <p>
+        Hello {person.name} your age is {person.age}
+      </p>
     </>
   );
 });
