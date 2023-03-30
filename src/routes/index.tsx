114aaafaa5d54eac2202074651fab59c2a3d634e
mark <markpackham1@gmail.com>
2023-03-30 14:50:25 +0100

    demo useSignal
    https://youtu.be/N624vxHocDc?t=142

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const name = useSignal("Bob");
+
   return (
     <>
       <h2>Hello World</h2>
+      <p>Hello {name.value}</p>
     </>
   );
 });
