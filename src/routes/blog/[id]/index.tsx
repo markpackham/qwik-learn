892a898c6fba3f78e35e38d31ce5eea5a7a1bec7
mark <markpackham1@gmail.com>
2023-03-31 18:41:10 +0100

    fetch single blog article based on id
    https://youtu.be/zm10kAKdF8I?t=345

--- src/routes/blog/[id]/index.tsx
+++ src/routes/blog/[id]/index.tsx
@@ -1,5 +1,21 @@
 import { component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
+
+interface BlogData {
+  id: string;
+  title: string;
+  content: string;
+}
+
+export const useBlogData = routeLoader$(async ({ params }) => {
+  const res = await fetch(`http://localhost:3000/blogs/${params.id}`);
+  const blogObj = (await res.json()) as BlogData;
+  return blogObj;
+});
 
 export default component$(() => {
+  const blog = useBlogData().value;
+  console.log(blog);
+
   return <>Hello World</>;
 });
