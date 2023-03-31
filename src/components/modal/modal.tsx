e09deec8c17efb2096e3901abab7ac0f37fb3021
mark <markpackham1@gmail.com>
2023-03-31 09:30:48 +0100

    use the Modal Styles
    https://youtu.be/1QW6Sd6PFIc?t=240

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,3 +1,17 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import ModalStyles from "./modal.css?inline";
 
-export default component$;
+export default component$(() => {
+  useStylesScoped$(ModalStyles);
+
+  return (
+    <div class="modal">
+      <div class="modal-content">
+        <div class="close">
+          <main class="main">Main modal content</main>
+          <footer>modal footer content</footer>
+        </div>
+      </div>
+    </div>
+  );
+});
